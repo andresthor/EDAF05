1dfbd58a3417b232bc97b6d5192bd82b2b3228ab
AntonFriberg <anton.friberg@outlook.com>
2016-04-01 08:41:02 +0200

    Added comment

--- eclipse/labs/lab1/src/GS.java
+++ eclipse/labs/lab1/src/GS.java
@@ -1,4 +1,6 @@
-import java.io.File;
+/*
+Implementation of the Gale-Shapley algorithm.
+*/import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -6,65 +8,65 @@ import java.util.Scanner;
 
 
 public class GS {
-	
+
 	static boolean DEBUG = true;
-	
+
 	public GS(Scanner input) {
 		ArrayList<Integer>[] menPref;
 		ArrayList<Integer>[] womenPref;
 		String[] names;
 		int[] partner;
 		LinkedList<Integer> freeMen;
-	
+
         // The first zero or more lines start with “#” and are ignored.
         while (input.hasNext("#*")) {
         	input.nextLine();
         }
-        
+
         // Then comes a line of the form “n=int” defining n.
         input = input.skip("n=");
         int n = input.nextInt();
         input.nextLine();
-        
+
         // The following 2n lines describe m1, w1, … mn, wn.
         names = new String[2*n];
         menPref = new ArrayList[n];
         womenPref = new ArrayList[n];
         freeMen = new LinkedList();
-        
+
         for (int i = 0; i < n; i++) {
         	menPref[i] = new ArrayList<Integer>();
         	womenPref[i] = new ArrayList<Integer>();
         }
-        
+
         for (int i = 0; i < n; i++) {
         	freeMen.addLast(i*2 + 1);
         }
-        
+
         for (int i = 0; i < 2*n; i++) {
         	names[i] = input.nextLine().split(" ")[1];
         	//System.out.println(names[i]);
         }
-        
+
         // Blank line
         input.nextLine();
-        
+
         // Finally, there is a line for every id (in order) that describes a
         // preference list. The line starts with id, followed by a colon and a
-        // single space. 
+        // single space.
         for (int i = 0; i < n; i ++) {
         	String[] manP = input.nextLine().split(" ");
         	String[] womanP = input.nextLine().split(" ");
-        	
+
         	for (int j = 0; j < n; j++) {
         		menPref[i].add(Integer.parseInt(manP[j+1]));
         		womenPref[i].add(Integer.parseInt(womanP[j+1]));
         	}
-        }            
+        }
 
         input.close();
-        
-        
+
+
         if (DEBUG) {
 	        // Print out the inputs
 	    	System.out.println("\nNames:");
@@ -85,11 +87,11 @@ public class GS {
 	        }
         }
 	}
-	
+
 	public void run() {
 		// do the algorithm here
 	}
-	
+
 
 	public static void main(String[] args) throws IOException {
 		GS gs;
@@ -103,4 +105,3 @@ public class GS {
 	    }
 	}
 }
-
