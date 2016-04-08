092b92b897e35549644f1be0a4dc57571eb46f88
AntonFriberg <anton.friberg@outlook.com>
2016-04-08 09:06:56 +0200

    Added new files for lab2

---
+++ eclipse/labs/lab2/src/world_ladder/Graph.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package world_ladder;
+
+/**
+ * @author anton
+ *
+ */
+public class Graph {
+
+}
---
+++ eclipse/labs/lab2/src/world_ladder/WordLadder.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package world_ladder;
+
+/**
+ * @author anton
+ *
+ */
+public class WordLadder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
