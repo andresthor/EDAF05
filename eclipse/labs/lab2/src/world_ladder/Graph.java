f538b4df50b93559a778bd4dc474cd90269a0f97
AntonFriberg <anton.friberg@outlook.com>
2016-04-08 09:45:00 +0200

    Added more files and started implementation

--- eclipse/labs/lab2/src/world_ladder/Graph.java
+++ eclipse/labs/lab2/src/world_ladder/Graph.java
@@ -3,6 +3,9 @@
  */
 package world_ladder;
 
+import java.util.HashMap;
+import java.util.TreeSet;
+
 /**  
  * Initializes a graph from an input stream.
  * The format is the number of vertices V,
@@ -14,7 +17,19 @@ package world_ladder;
  * @throws IllegalArgumentException if the number of vertices or edges is negative
  */
 public class Graph {
-	private HashMap<Vertex, TreeSet<Vertex>> neighbo
+	private HashMap<Vertex, TreeSet<Vertex>> neighbors;
+	private HashMap<String, Vertex> myVertices;
+	private static final TreeSet<Vertex> EMPTY_SET = new TreeSet<Vertex>();
+	private int myNumVertices;
+	private int myNumEdges;
 	
+	/*
+	 * Construct new empty Graph
+	 */
+	public Graph() {
+		neighbors = new HashMap<Vertex, TreeSet<Vertex>>();
+		myVertices
+		myNum
+	}
 	
 }
