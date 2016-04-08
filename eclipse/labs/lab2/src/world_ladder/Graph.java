fadd5870a6cd9f6a27c65d5d776ddbb36ef67af4
AntonFriberg <anton.friberg@outlook.com>
2016-04-08 09:31:05 +0200

    Added init files

--- eclipse/labs/lab2/src/world_ladder/Graph.java
+++ eclipse/labs/lab2/src/world_ladder/Graph.java
@@ -3,10 +3,18 @@
  */
 package world_ladder;
 
-/**
- * @author anton
+/**  
+ * Initializes a graph from an input stream.
+ * The format is the number of vertices V,
+ * followed by the number of edges E,
+ * followed by E pairs of vertices, with each entry separated by whitespace.
  *
+ * @param  in the input stream
+ * @throws IndexOutOfBoundsException if the endpoints of any edge are not in prescribed range
+ * @throws IllegalArgumentException if the number of vertices or edges is negative
  */
 public class Graph {
-
+	private HashMap<Vertex, TreeSet<Vertex>> neighbo
+	
+	
 }
