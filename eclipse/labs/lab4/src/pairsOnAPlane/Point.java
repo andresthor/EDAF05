17f4b0f3de76cfbd25e75e7f61277aa9276f75d0
AntonFriberg <anton.friberg@outlook.com>
2016-04-28 12:14:54 +0200

    Simple point implementation

--- eclipse/labs/lab4/src/pairsOnAPlane/Point.java
+++ eclipse/labs/lab4/src/pairsOnAPlane/Point.java
@@ -1,5 +1,20 @@
 package pairsOnAPlane;
 
-public class Point {
+public class Point implements Comparable<Point> {
+	public int x;
+	public int y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 
+	/* 
+	 * Overrides compareTo so that only x values is compared.
+	 * Orders points by increasing x values.
+	 */
+	@Override
+	public int compareTo(Point that) {
+		return this.x - that.x;
+	}
 }
